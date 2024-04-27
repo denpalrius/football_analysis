e180e95343da1443386db2bb45b8ef78ab544e82
Dennis Riungu <2971971+denpalrius@users.noreply.github.com>
2024-04-26 19:27:52 -0500

    Load YOLO v8 model

---
+++ yolo_inference.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+model = YOLO(model='yolov8x')
+
+results = model.predict('./input_videos/08fd33_4.mp4', save = True, save_dir = './output_videos')
+
+print(results[0])
+print("====================================")
+for box in results[0].boxes:
+    print(box)
